f9eebbd15f6cb0b7f77d0e124ed4318aaeb8a40d
Kayla New <kayla.new.n@gmail.com>
2023-08-10 20:20:25 -0700

    login

--- NewbiesTodo/screens/LoginScreen.js
+++ NewbiesTodo/screens/LoginScreen.js
@@ -1,20 +1,33 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleLogin = () => {
-        
+        setEmailError('');
+        setPasswordError('');
+        if (!email) {
+            setEmailError('Please fill in email');
+
+        }
+        if (!password) {
+            setPasswordError('Please fill in password');
+            return;
+        }
     }
 
     return (
         <View>
             <Text>Login Screen</Text>
-            <TextInput placeholder='Email...' onChangeText={text => setEmail({text})} />
-            <TextInput placeholder='Password...' onChangeText={text => setPassword({text})} />
-            <Button title="Login" />
+            <TextInput placeholder='Email...' onChangeText={text => setEmail({ text })} />
+            {emailError && <Text>{emailError}</Text>}
+            <TextInput placeholder='Password...' onChangeText={text => setPassword({ text })} />
+            {passwordError && <Text>{passwordError}</Text>}
+            <Button title="Login" onPress={(handleLogin)} />
             <Button title="Forgot Password / Sign Up" />
         </View>
     )
